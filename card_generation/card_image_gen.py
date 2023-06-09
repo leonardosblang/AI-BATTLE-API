1ba811a00b2b9ed0891e5527a855be1cab4158a5
Leolang <leolang99@gmail.com>
2023-06-08 21:40:08 -0300

    update card maker

--- card_generation/card_image_gen.py
+++ card_generation/card_image_gen.py
@@ -1,62 +1,68 @@
-from PIL import Image, ImageDraw, ImageFont, ImageOps
-
-def colorize(image: Image.Image, color: str) -> Image.Image:
-    # Convert the image to grayscale
-    gray = image.convert('L')
-
-    # Colorize the grayscale image with the given color
-    result = ImageOps.colorize(gray, "black", color)
-
-    return result.convert('RGBA')
+from PIL import Image, ImageDraw, ImageFont
 
 def create_card(rarity: str, image_path: str, card_name: str, card_description: str, card_cost: str):
-    # Define colors based on rarity
-    rarity_colors = {"common": '#FFFFFF',  # no change
-                     "uncommon": '#0000FF',  # blue
-                     "rare": '#800080',  # purple
-                     "legendary": '#FFD700'}  # gold
+    # Define images based on rarity
+    rarity_images = {"common": 'bg.png',
+                     "uncommon": 'bguncommon.png',
+                     "rare": 'bgrare.png',
+                     "legendary": 'bglegendary.png'}
 
     # Open the base image
-    base_img = Image.open('bg.png')
+    base_img = Image.new('RGBA', (120, 200))
 
-    # If the image is not RGBA, convert it
-    if base_img.mode != 'RGBA':
-        base_img = base_img.convert('RGBA')
+    # Open and place the bg1 image
+    bg1_img = Image.open('bg1.png')
+    if bg1_img.mode != 'RGBA':
+        bg1_img = bg1_img.convert('RGBA')
 
-    # Colorize the base image based on rarity
-    if rarity != "common":  # common won't change color
-        base_img = colorize(base_img, rarity_colors[rarity])
+    base_img.paste(bg1_img, (0, 0), bg1_img)
 
     # Open and resize the card image
-    card_img = Image.open(image_path).resize((128, 128))
+    card_img = Image.open(image_path).resize((120, 150))
 
     # If the image is not RGBA, convert it
     if card_img.mode != 'RGBA':
         card_img = card_img.convert('RGBA')
 
     # Place the card image at the center of the base image
-    base_img.paste(card_img, (-5, 25))
+    base_img.paste(card_img, (0, 0))  # Adjusted to place image 'a' at center
+
+    # Open the bg image based on rarity
+    bg_img = Image.open(rarity_images[rarity])
+    if bg_img.mode != 'RGBA':
+        bg_img = bg_img.convert('RGBA')
+
+    base_img.paste(bg_img, (0, 0), bg_img)
+
+    # Open the bg3 image
+    bg3_img = Image.open('bg3.png')
+    if bg3_img.mode != 'RGBA':
+        bg3_img = bg3_img.convert('RGBA')
+
+    base_img.paste(bg3_img, (0, 0), bg3_img)
 
     # Initialize the drawing context
     draw = ImageDraw.Draw(base_img)
 
-    # Define the font (assuming that arial.ttf is in your working directory)
-    font = ImageFont.truetype('arial.ttf', 15)
+    # Define the font (assuming that 'unispace bd.otf' is in your working directory)
+    font = ImageFont.truetype('unispace bd.otf', 8)
 
-    ellipse_position = (90, 0, 115, 25)  # Adjusted bounding box for a circle
-    draw.ellipse(ellipse_position, fill='blue')
+
 
     # Draw the card name above the image
-    draw.text((10, 5), card_name, fill='white', font=font)
-
-    # Draw the cost text on the blue circle
-    draw.text((100, 5), card_cost, fill='white', font=font)  # Adjusted position for cost text
+    draw.text((50, 10), card_name, fill='white', font=font)
 
     # Draw the card description below the image
     draw.text((5, 160), card_description, fill='white', font=font)
+
+
+
+    font = ImageFont.truetype('unispace bd.otf', 12)
+    # Draw the cost text on the blue circle
+    draw.text((12, 11), card_cost, fill='white', font=font)  # Adjusted position for cost text
 
     # Save the image
     base_img.save('card.png')
 
 # Use the function
-create_card('rare', 'a.png', 'Card Name', 'Card Description', '3')
+create_card('rare', 'a.png', 'Card Name', 'Card Description', '33')
